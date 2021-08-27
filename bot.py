97b968d5fca7a510554e9dfe4d3cd0cc060ba245
kamath7 <adithyakamath96@gmail.com>
2021-08-28 01:29:05 +0530

    Initial message posting with slack

---
+++ bot.py
@@ -0,0 +1,11 @@
+#following techwithtims tuts
+import os
+import slack
+from pathlib import Path 
+from dotenv import load_dotenv 
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client.chat_postMessage(channel="#nothing-much",text="Shaktimaaaaan!!")
